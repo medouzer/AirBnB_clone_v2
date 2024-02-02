9ff6b150ad537d7611b6c0deba6d867c90cdad9b
mohamed ouzerzou <mohamed.ouz20016@gmail.com>
2024-02-01 18:29:52 -0800

    new task

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Compress before sending"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """do pack"""
+    if not os.path.exists("versions"):
+        local("mkdir -p versions")
+
+    now = datetime.utcnow()
+    date_time = now.strftime("%Y%m%d%H%M%S")
+    path_arch = "versions/{}".format(date_time)
+    archive = local("tar -cvzf {} web_static".format(path_arch))
+
+    if archive.succeeded:
+        return archive_path
+    else:
+        return None
