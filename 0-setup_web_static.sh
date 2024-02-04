e20ba5e0b78fcf41f33b89061ecaf75a0e097aa3
mohamed ouzerzou <mohamed.ouz2016@gmai.com>
2024-02-04 15:12:55 -0800

    fix some errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -24,6 +24,8 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 config_path=/etc/nginx/sites-available/default
-sudo sed -i "s@^\tlocation / {@\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}\n\n\tlocation / {@g" $config_path
+if ! grep -q "location /hbnb_static/ {" "$config_path"; then
+	sudo sed -i "s@^\tlocation / {@\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}\n\n\tlocation / {@g" $config_path
+fi
 
 sudo service nginx restart
