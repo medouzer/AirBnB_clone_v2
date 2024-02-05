83762f3f3f6c66509aa81bd42d11d0f85488ad9c
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-04 16:17:58 -0800

    new things

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-"""Deploy archive!"""
-from fabric.api import *
-from os.path import exists
+"""Compress before sending"""
+from fabric.api import local
+from datetime import datetime
+import os
 
 
-env.hosts = ['54.152.181.77', '100.25.220.209']
+@task
+def do_pack():
+    """do pack"""
+    if not os.path.exists("versions"):
+        local("mkdir -p versions")
+
+    now = datetime.utcnow()
+    date_time = now.strftime("%Y%m%d%H%M%S")
+    path_arch = "versions/web_static_{}.tgz".format(date_time)
+    archive = local("tar -cvzf {} web_static".format(path_arch))
+
+    if archive.succeeded:
+        return path_arch
+    else:
+        return None
 
 
+@task
 def do_deploy(archive_path):
     """do_deploy method"""
     if not exists(archive_path):
@@ -25,3 +41,12 @@ def do_deploy(archive_path):
     run("rm -rf {}".format(current_path))
     run("ln -s {} {}".format(release_path, current_path))
     return True
+
+
+@task
+def deploy():
+    """ deploy method"""
+    path = do_pack()
+    if path is None:
+        return False
+    return do_deploy(path)
