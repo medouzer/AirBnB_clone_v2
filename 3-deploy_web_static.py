b6ca0ed534bcf8291de60afca38892f4ed9d0836
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-04 16:50:11 -0800

    errors

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,6 @@ from os.path import exists
 env.hosts = ['54.152.181.77', '100.25.220.209']
 
 
-@runs_once
 def do_pack():
     """do pack"""
     if not os.path.exists("versions"):
@@ -26,28 +25,29 @@ def do_pack():
         return None
 
 
-@task
 def do_deploy(archive_path):
     """do_deploy method"""
-    if not exists(archive_path):
+    try:
+        if not exists(archive_path):
+            return False
+        file_arch = archive_path.split('/')[-1]
+        name_arch = file_arch.split('.')[0]
+        tmp_path = "/tmp/{}".format(file_arch)
+        release_path = "/data/web_static/releases/{}".format(name_arch)
+        current_path = "/data/web_static/current"
+        put(archive_path, tmp_path)
+        run("mkdir -p {}".format(release_path))
+        run("tar -xzf {} -C {}".format(tmp_path, release_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}/web_static/* {}".format(release_path, release_path))
+        run("rm -rf {}/web_static".format(release_path))
+        run("rm -rf {}".format(current_path))
+        run("ln -s {} {}".format(release_path, current_path))
+        return True
+    except Exception:
         return False
-    file_arch = archive_path.split('/')[-1]
-    name_arch = file_arch.split('.')[0]
-    tmp_path = "/tmp/{}".format(file_arch)
-    release_path = "/data/web_static/releases/{}".format(name_arch)
-    current_path = "/data/web_static/current"
-    put(archive_path, tmp_path)
-    run("mkdir -p {}".format(release_path))
-    run("tar -xzf {} -C {}".format(tmp_path, release_path))
-    run("rm {}".format(tmp_path))
-    run("mv {}/web_static/* {}".format(release_path, release_path))
-    run("rm -rf {}/web_static".format(release_path))
-    run("rm -rf {}".format(current_path))
-    run("ln -s {} {}".format(release_path, current_path))
-    return True
 
 
-@task
 def deploy():
     """ deploy method"""
     path = do_pack()
