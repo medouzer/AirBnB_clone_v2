579ea0304a79b58f3283124ff1ce8040e511912a
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-17 10:14:32 +0100

    fix and test on sandbox

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
-from models import storage
+from models.engine.db_storage import storage
 
 t_storage = getenv("HBNB_TYPE_STORAGE")
 
