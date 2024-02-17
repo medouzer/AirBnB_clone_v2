f4a0c0b200d137c955fe7a44a70b999f716585ee
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-17 04:53:37 +0100

    to test

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,7 +15,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_dir(text):
-    return 'C {}'.format(text.split("_"))
+    return 'C {}'.format(text.replace("_", " "))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
