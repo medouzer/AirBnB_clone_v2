b070914b175a2e35d8f9d5d7b0c312ff22734b0a
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-17 09:12:18 +0100

    fix errors

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """ C is fun! """
-
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_dir(text):
     return 'C {}'.format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """ Python is cool! """
-
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_dir(text):
     return 'C {}'.format(text.replace("_", " "))
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -24,6 +27,7 @@ def python_dir(text=""):
         return "Python is cool"
     else:
         return 'Python {}'.format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
