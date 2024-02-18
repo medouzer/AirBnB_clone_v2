1b69b1d2d0bb11ef34bade0310bee1d199143f60
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-18 09:56:14 +0100

    to fix

--- models/city.py
+++ models/city.py
@@ -10,10 +10,6 @@ t_storage = getenv("HBNB_TYPE_STORAGE")
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    if t_storage == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship('Place', cascade="all,delete", backref="cities")
-    else:
-        state_id = ""
-        name = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', cascade="delete", backref="cities")
